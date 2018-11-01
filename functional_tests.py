2eec0dbed4ba8cd0ed31b4c9cbc73474ee982cf9
shimaamohamed92 <shimaa.mo7amed92@gmail.com>
2018-11-01 15:00:24 +0200

    update unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,19 +1,20 @@
-from django.urls import resolve
-from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
+from selenium import webdriver
+import unittest
 
 
-class HomePageTest(TestCase):
+class NewVisitorTest(unittest.TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswitch('<html'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswitch('</html>'))
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	def tearDown(self):
+		self.browser.quit()
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		# She is invited to enter a to-do item straight away
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
